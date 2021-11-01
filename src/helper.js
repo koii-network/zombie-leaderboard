e066833cf9d9b1b83ec67784f8dc55f2ce39973b
Andre Vallestero <39736205+AndreVallestero@users.noreply.github.com>
2021-11-01 12:13:26 -0400

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,7 @@
 import zombieList from "./zombies.json";
-import { isEmpty, keys } from "lodash";
 
 export async function mapNftToZombies() {
   const nfts = await (await fetch("https://mainnet.koii.live/attention/latest")).json();
-  const nKeys = keys(nfts);
   zombieList.sort((a, b) => (nfts[b] || 0) - (nfts[a] || 0));
   const zombies = zombieList.slice(0, 10);
 
